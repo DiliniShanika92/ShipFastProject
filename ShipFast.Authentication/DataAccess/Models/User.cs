0841b0270bf54b8af72bf8dda2dc313de46f9539
Dilini Shanika <diliniwickramathunga@gmail.com>
2024-10-30 22:44:52 +0530

    Add data access to auth service

---
+++ ShipFast.Authentication/DataAccess/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace ShipFast.Authentication.DataAccess.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
--- ShipFast.Authentication/Program.cs
+++ ShipFast.Authentication/Program.cs
@@ -1,10 +1,32 @@
+//using DotnetRDS.Data;
+using Microsoft.EntityFrameworkCore;
+using ShipFast.Authentication.DataAccess;
+using ShipFast.Authentication.DataAccess.Models;
+using System.Reflection.Metadata;
+
 var builder = WebApplication.CreateBuilder(args);
+
+ConfigureServices(
+    builder.Services,
+    builder.Configuration
+);
 
 var app = builder.Build();
 
-app.MapGet("/observation/{zip}", (string zip) =>
+app.MapPost("/authentication", async(AuthDbContext authDbContext, User user) =>
 {
-    return Results.Ok(zip);
-});
+    await authDbContext.Users.AddAsync(user);
+    await authDbContext.SaveChangesAsync();
+    return Results.Created($"/authentication/{user.Id}", user);
+}).Produces(StatusCodes.Status201Created);
 
 app.Run();
+
+void ConfigureServices(IServiceCollection services, ConfigurationManager configurationManager)
+{
+    services.AddDbContext<AuthDbContext>(
+        opts =>
+        {
+            opts.UseNpgsql(configurationManager.GetConnectionString("AuthDb"));
+        }, ServiceLifetime.Transient);
+}
